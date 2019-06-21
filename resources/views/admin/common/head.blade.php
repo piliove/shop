3bc434f2a32a1ca775aa02c018fd561b4a64e755
muyinya <muyinya@163.com>
2019-06-21 14:55:26 +0800

    头部文件

--- resources/views/admin/common/head.blade.php
+++ resources/views/admin/common/head.blade.php
@@ -26,7 +26,6 @@
 </head>
 <body>
 <div class="container-scroller">
-    <!-- partial:partials/_navbar.html -->
     <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
             <a class="navbar-brand brand-logo" href="index.html"><img src="/admins/images/logo.svg" alt="logo"/></a>
